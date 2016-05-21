3962602a6946f7542062247c685ef5df8df620d3
Eisha293 <eishatahir@gmail.com>
2016-05-21 12:10:19 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,21 @@
-import socket  # Import socket module
+from socket import *   # Import socket module
+from threading import Thread
+impoort sys
 
-s = socket.socket()
-host = socket.gethostname()
+s = socket(AF_INET,SOCK_STREAM)
+host = 'localhost'
 port = 5188  # Reserve a port for your service.
 
 s.connect((host, port))
-print s.recv(1024)
+def recv ():
+  while True:
+    data=s.recv(1024)
+    if not data:
+      sys.exit(0)
+    print data
+Thread (target=recv).start()
+while True:
+  data= raw_input(' >> ')
+  s.send(data)
+  
 s.close  # Close the socket when done
